76347289801930ad2cdcee7a3efac46001e159a4
Matthias von Arx <mavonarx@student.ethz.ch>
2021-08-01 13:18:37 +0200

    Heaps

--- module/src/DataStructures/Heap.java
+++ module/src/DataStructures/Heap.java
@@ -1,20 +1,22 @@
 package DataStructures;
 
 import java.util.Arrays;
+@SuppressWarnings("unchecked")
 public class Heap<T extends Comparable<T>>  implements HeapInterface<T> {
     private int size;
     private T [] heap;
     private static final int DEFAULT_CAPACITY=1000;
     public int capacity;
-    public boolean min=true;
+    public boolean minHeap =true;
 
     public Heap(){
+
         heap= (T[]) new Comparable[DEFAULT_CAPACITY];
         capacity=DEFAULT_CAPACITY;
     }
 
     public Heap(boolean type){
-        min=type;
+        minHeap =type;
     }
 
     public Heap(int maxCapacity){
@@ -24,7 +26,7 @@ public class Heap<T extends Comparable<T>>  implements HeapInterface<T> {
 
     public Heap (int maxCapacity, boolean type){
         this(maxCapacity);
-        min = type;
+        minHeap = type;
     }
 
     public T [] get(){
@@ -32,96 +34,106 @@ public class Heap<T extends Comparable<T>>  implements HeapInterface<T> {
     }
 
     public void insert(T value) {
-        if (size>capacity){
+        if (size>=capacity){
             throw new RuntimeException("Heap is full");
         }
-        heap[size]= value;
-        heapify(size);
         size++;
+        heap[size-1]= value;
+        bubbleUp(size-1);
     }
 
-    private void heapify(int position){
+    private void bubbleUp(int position){
         if (position==0){
             return;
         }
 
-        if (heap[position].compareTo(heap[((position-1)/2)]) <0 && min){
-            change(position,(position-1)/2);
-            heapify((position-1)/2);
+        //minHeap
+        if (heap[position].compareTo(heap[((position-1)/2)]) <0 && minHeap){
+            swap(position,(position-1)/2);
+            bubbleUp((position-1)/2);
             return;
         }
 
-        if (heap[position].compareTo(heap[((position-1)/2)]) >0 && !min){
-            change(position,(position-1)/2);
-            heapify((position-1)/2);
+        //maxHeap
+        if (heap[position].compareTo(heap[((position-1)/2)]) >0 && !minHeap){
+            swap(position,(position-1)/2);
+            bubbleUp((position-1)/2);
         }
     }
 
     @Override
     public boolean isEmpty() {
-        return (getSize()==0);
+        return (getSize()<=0);
     }
 
     public T remove(){
         return poll();
     }
 
-    private void change(int position1, int position2){
+    private void swap(int position1, int position2){
         T temp = heap[position1];
         heap[position1]=heap[position2];
         heap[position2]=temp;
     }
 
-    // checks wich of the childs is bigger - if only 1 child exists return that. if no child exists return -1
-    private int biggerChild(int position){
+    // checks witch of the children is bigger - if only 1 child exists return that. if no child exists return -1
+    private int getBiggerChild(int position){
+        int leftChild = position*2+1;
+        int rightChild = position*2+2;
 
         // no child exists
-        if (position * 2 + 2 > size){
+        if (rightChild > size){
             return -1;
         }
 
         // only left child exists
-        if (position * 2 +1 == size){
-
-            return position*2+1;
-
+        if (leftChild == size){
+            return leftChild;
         }
 
         // left child bigger
-        if (heap[(position*2+1)].compareTo(heap[(position*2+2)])<0 && min){
-            return position*2+1;
+            // in a MinHeap
+        if (heap[(leftChild)].compareTo(heap[(rightChild)])<0 && minHeap){
+            return leftChild;
         }
 
-        if (heap[(position*2+1)].compareTo(heap[(position*2+2)])>0 && !min){
-            return position*2+1;
+            // in a MaxHeap
+        if (heap[(leftChild)].compareTo(heap[(rightChild)])>0 && !minHeap){
+            return leftChild;
         }
         // right child bigger
         else{
-            return position*2+2;
+            return rightChild;
         }
+    }
+
+    private boolean childExists(int position){
+        return getBiggerChild(position) != -1;
     }
 
     private void sicker(int position){
-
         // child exists
-        if (!(biggerChild(position)==-1)){
-            int a = biggerChild(position);
-            if (heap[a].compareTo(heap[position])<0 && min){
-                change(position, a);
-                sicker(a);
+        if (childExists(position)){
+            int biggerChild = getBiggerChild(position);
+            // swaps the values if the child has more priority - then recursively goes down the tree
+                // in a MinHeap
+            if (heap[biggerChild].compareTo(heap[position])<0 && minHeap){
+                swap(position, biggerChild);
+                sicker(biggerChild);
                 return;
             }
-
-            if (heap[a].compareTo(heap[position])>0 && !min){
-                change(position, a);
-                sicker(a);
+                // in a MaxHeap
+            if (heap[biggerChild].compareTo(heap[position])>0 && !minHeap){
+                swap(position, biggerChild);
+                sicker(biggerChild);
             }
         }
     }
 
+
     public T poll(){
-        if (size<=0){
-            throw new RuntimeException("Queue is empty - nothing to poll here");
+        if (isEmpty()){
+            throw new RuntimeException("Queue is empty");
         }
         T result = heap[0];
         heap[0]=heap[size-1];
@@ -148,6 +160,10 @@ public class Heap<T extends Comparable<T>>  implements HeapInterface<T> {
 
     @Override
     public String toString(){
+        return Arrays.toString(heap);
+    }
+
+    public String getHeapAsString(){
         return Arrays.toString(heap);
     }
 
