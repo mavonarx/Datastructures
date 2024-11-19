740d52b9d1bbad9aa514ad8baa76341e2d6603e8
Matthias von Arx <matthias.von-arx@gmx.ch>
2024-11-19 09:36:35 +0100

    removed my lib

--- Rust/mylib/target/debug/deps/mylib-8e5360c2fea40e1c.d
+++
@@ -1,5 +0,0 @@
-/Users/matthiasvonarx/Nextcloud/Projects/Datastructures/Rust/mylib/target/debug/deps/mylib-8e5360c2fea40e1c: src/lib.rs
-
-/Users/matthiasvonarx/Nextcloud/Projects/Datastructures/Rust/mylib/target/debug/deps/mylib-8e5360c2fea40e1c.d: src/lib.rs
-
-src/lib.rs:
